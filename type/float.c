073d75825d4895075983b32d430c172f30be5cec
= <=>
2017-12-14 14:04:01 +0800

    12114

--- type/float.c
+++ type/float.c
@@ -3,7 +3,10 @@
 
 int main()
 {
-	// printf("size: %d\n",sizeof(float) );
+	printf("int size: %lu\n",sizeof(int) );
+	printf("float size: %lu\n", sizeof(float));
+	printf("double size: %lu\n", sizeof(double));
+	printf("char size: %lu\n", sizeof(char));
 	printf("min: %E\n", FLT_MIN);
 	printf("max: %E\n", FLT_MAX);
 	printf("Precision value: %d\n", FLT_DIG);
